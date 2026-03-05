eb886060bd25c614f6c84f59b03731d29784dbc8
joelgamba04 <joelgamba04@gmail.com>
2026-03-05 14:40:41 +0800

    Added Icons on Tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,36 @@
 // app/(tabs)/_layout.tsx
 
+import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{ headerTitleAlign: "center" }}>
+    <Tabs
+      screenOptions={({ route }) => ({
+        headerTitleAlign: "center",
+        tabBarShowLabel: true,
+        tabBarIcon: ({ focused, color, size }) => {
+          const icon = getTabIcon(route.name, focused);
+          return <Ionicons name={icon} size={size} color={color} />;
+        },
+      })}
+    >
       <Tabs.Screen name="index" options={{ title: "Recipes" }} />
       <Tabs.Screen name="create" options={{ title: "Create" }} />
       <Tabs.Screen name="settings" options={{ title: "Settings" }} />
     </Tabs>
   );
 }
+
+const getTabIcon = (name: string, focused: boolean) => {
+  switch (name) {
+    case "index":
+      return focused ? "bookmark" : "bookmark-outline"; // Recipes
+    case "create":
+      return focused ? "add-circle" : "add-circle-outline"; // Create
+    case "settings":
+      return focused ? "settings" : "settings-outline"; // Settings
+    default:
+      return focused ? "ellipse" : "ellipse-outline";
+  }
+};
